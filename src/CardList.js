79a9d136b7cda404389b87352c67685bd295ecf4
ederson badeca <edersonbadeca@gmail.com>
2018-11-29 19:25:55 -0200

    :lipstick: - javascript

--- src/CardList.js
+++ src/CardList.js
@@ -1,25 +1,20 @@
-import React, { Component }  from 'react'
-import Card from './Card'
+import React, { Component }  from 'react';
+import Card from './Card';
 
 
 class CardList  extends Component {
 
 
-	constructor(props) {
-			super(props);
-
-		}
-
   render(){
 
-    console.log(this.props.robots)
-    const cardComponent = this.props.robots.map((user, index) =>
+    const robots = this.props.robots;
+    const cardComponent = robots.map((user, index) =>
        <Card
          key={index}
-         id={ this.props.robots[index].id }
-         name={ this.props.robots[index].name }
-         username={ this.props.robots[index].name }
-         email={ this.props.robots[index].email }
+         id={ robots[index].id }
+         name={ robots[index].name }
+         username={ robots[index].name }
+         email={ robots[index].email }
          />
       )
 
@@ -32,4 +27,4 @@ class CardList  extends Component {
   }
 }
 
-export default CardList
+export default CardList;
