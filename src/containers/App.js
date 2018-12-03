ba07ffdce8e8c06b3999506aa36da7fd83fa633b
ederson badeca <edersonbadeca@gmail.com>
2018-12-03 11:53:17 -0200

    Clean up the workspace

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 
-import CardList from './CardList'
-import SearchBox from './SearchBox'
-import './App.css'
-import Scroll from './Scroll'
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox'
+import Scroll from '../components/Scroll'
+
+import '../containers/App.css'
 
 class App extends Component {
   constructor(){
